f7cea206e57ea373decd89941e2165f22517ad13
thisLucio <luxusnietzsche@gmail.com>
2021-09-13 23:19:48 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/devlucio/dlvendas/DlvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devlucio.dlvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devlucio/dlvendas/DlvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucio.dlvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
